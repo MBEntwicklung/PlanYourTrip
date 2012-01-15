7fb400742fb0c3732b106d6ee7f5dc7577489b33
Marc Bellmann <marc.bellmann@mb-entwicklung.de>
2012-01-15 15:38:42 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanYourTrip::Application.config.secret_token = '8706006fab2bf4cfa1086a4e73fb3859b537f8195a131a73e1c57edfcd3e50fe63740e444650ff1e81302035c5ff544eddc9ef0ec884bd742e4373ddd0200618'
